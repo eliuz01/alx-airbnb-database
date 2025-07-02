1fbb0d152f22c01e8ce4c597204ab0e57eabc896
ELIUZ <osongoeliuz14@gmail.com>
2025-07-02 22:51:00 +0300

    corrected left join by adding Order By

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,7 +4,8 @@ INNER JOIN users ON bookings.user_id = users.user_id;
 
 SELECT properties.property_id, properties.name AS property_name, properties.description, properties.location, properties.price_per_night, reviews.review_id, reviews.user_id AS reviewer_id, reviews.rating, reviews.comment, reviews.created_at AS review_date
 FROM properties
-LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+LEFT JOIN reviews ON properties.property_id = reviews.property_id
+ORDER BY properties.property_id;
 
 SELECT users.user_id, users.first_name, users.last_name, bookings.booking_id, bookings.property_id, bookings.start_date, bookings.end_date, bookings.status
 FROM users
