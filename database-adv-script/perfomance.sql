271d349c80f09b276d68baf639edcc3dfecea975
ELIUZ <osongoeliuz14@gmail.com>
2025-07-03 08:21:36 +0300

    correted perfoamnce.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -41,20 +41,24 @@ JOIN properties p ON b.property_id = p.property_id
 LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
 
 --Optimized Version of the Query
-SELECT 
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.status,
-    u.first_name || ' ' || u.last_name AS user_name,
-    u.email,
-    p.name AS property_name,
-    p.location,
-    pay.amount,
-    pay.payment_method
+SELECT
+  b.booking_id,
+  b.start_date,
+  b.end_date,
+  b.status,
+  u.user_id,
+  u.first_name || ' ' || u.last_name AS full_name,
+  p.property_id,
+  p.name AS property_name,
+  pay.payment_id,
+  pay.amount,
+  pay.payment_method
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
 LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND pay.amount > 1000
 ORDER BY b.created_at DESC
-LIMIT 100;
+LIMIT 50;
+
